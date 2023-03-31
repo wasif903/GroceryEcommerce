d03ce97016d31f76603d0dabcf673a19cc98ab0a
unknown <wasifmehmood903@gmail.com>
2023-04-01 04:08:27 +0500

    pushing

--- Backend/routes/categoryCRUD.js
+++ Backend/routes/categoryCRUD.js
@@ -3,13 +3,14 @@ const Category = require('../models/category');
 const router = require('express').Router();
 const authMiddleware = require('../middlewares/authMiddleware');
 
-router.post('/create-category', authMiddleware(['Manager', 'Employee']), async (req, res) => {
+router.post('/create-category', authMiddleware([ 'Admin' ,'Manager', 'Employee']), async (req, res) => {
   try {
     const createCategory = await new Category(req.body);
     const saveCategory = await createCategory.save();
     res.status(201).json(saveCategory);
   } catch (error) {
     res.status(500).json({ message: 'Internal Server Error' });
+    console.log(error)
   }
 });
 
